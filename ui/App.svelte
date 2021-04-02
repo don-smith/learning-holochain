2870d8408feed1417b373ec7a20a6d8112815c45
Don Smith <hi@dons.dev>
2021-04-03 12:37:53 +1300

    add start of ui

---
+++ ui/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  let greeting = ''
+  function handleClick () {
+    greeting = 'Hello from Svelte'
+  }
+</script>
+<div>
+  <button on:click={handleClick}>Say hello</button>
+  <p>Greeting: {greeting}</p>
+</div>
