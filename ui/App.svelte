431ef05dbf050b9a135953f46ba0f79a5256e347
Don Smith <hi@dons.dev>
2021-04-06 02:18:51 +1200

    get message from conductor

--- ui/App.svelte
+++ ui/App.svelte
@@ -1,40 +1,31 @@
 <script>
+  import Buffer from 'buffer'
   import { AppWebsocket } from '@holochain/conductor-api'
 
+  window.Buffer = Buffer.Buffer
+
   let greeting = ''
-  const timeout = 12000
 
   function handleClick () {
-    console.log('handling the click')
     getGreeting()
   }
 
   async function getGreeting () {
-    const client = await AppWebsocket.connect('ws://localhost:8888', timeout, signalCb)
+    const appConnection = await AppWebsocket.connect('ws://localhost:8888')
+    const appInfo = await appConnection.appInfo({ installed_app_id: 'test-app' })
+    const cellId = appInfo.cell_data[0].cell_id
 
-    console.log('got here')
-
-    await client.callZome({
+    const message = await appConnection.callZome({
      cap: null,
-     cell_id: [
-       'uhC0k4QpaOlFB3A5xZoT3m4C7jA2juCxpQtNAZwg2HEoRr7r-wcix'.split(''),
-       'uhCAkMSUebo8J45NgrKgO--oDEVDBYuW2rhr_1rq8wDLR-v_90PK'.split('')
-     ],
+     cell_id: cellId,
      zome_name: 'greeter',
      fn_name: 'hello',
-     provenance: 'uhCAkMSUebo8J45NgrKgO--oDEVDBYuW2rhr_1rq8wDLR-v_90PK'.split(''),
+     provenance: cellId[1],
      payload: null,
-    }, timeout)
-  }
+    })
 
-  function signalCb (signal) {
-    console.log('signal:', signal)
-    greeting = 'Hello from Svelte'
+    greeting = message
   }
-
-    function makeBuffer (str) {
-      return str.split('')
-    }
 </script>
 <div>
   <button on:click={handleClick}>Say hello</button>
