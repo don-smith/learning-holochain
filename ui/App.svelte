f6b181e2a1d5b04efa788c5fb5d0a5e0f7955488
Don Smith <hi@dons.dev>
2021-04-05 23:23:59 +1200

    add happ bundle

--- ui/App.svelte
+++ ui/App.svelte
@@ -2,26 +2,39 @@
   import { AppWebsocket } from '@holochain/conductor-api'
 
   let greeting = ''
+  const timeout = 12000
 
-  async function handleClick () {
-    const timeout = 12000
-    const client = await AppWebsocket.connect('http://localhost:8888', timeout, signalCb)
+  function handleClick () {
+    console.log('handling the click')
+    getGreeting()
+  }
+
+  async function getGreeting () {
+    const client = await AppWebsocket.connect('ws://localhost:8888', timeout, signalCb)
+
+    console.log('got here')
 
     await client.callZome({
      cap: null,
-     cell_id,
+     cell_id: [
+       'uhC0k4QpaOlFB3A5xZoT3m4C7jA2juCxpQtNAZwg2HEoRr7r-wcix'.split(''),
+       'uhCAkMSUebo8J45NgrKgO--oDEVDBYuW2rhr_1rq8wDLR-v_90PK'.split('')
+     ],
      zome_name: 'greeter',
      fn_name: 'hello',
-     provenance: fakeAgentPubKey('TODO'),
+     provenance: 'uhCAkMSUebo8J45NgrKgO--oDEVDBYuW2rhr_1rq8wDLR-v_90PK'.split(''),
      payload: null,
     }, timeout)
-
   }
 
   function signalCb (signal) {
+    console.log('signal:', signal)
     greeting = 'Hello from Svelte'
-    resolve()
   }
+
+    function makeBuffer (str) {
+      return str.split('')
+    }
 </script>
 <div>
   <button on:click={handleClick}>Say hello</button>
